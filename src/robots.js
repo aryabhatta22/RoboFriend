7a3e2b1334ca1eab8b7671e48d5c7edf2995cef5
Tarun Sharma <tarun.hive@hotmail.com>
2019-06-05 13:15:00 +0530

    Initial commits for RoboFriend Project

---
+++ src/robots.js
@@ -0,0 +1,74 @@
+export const robots=[
+    {
+        id: 1,
+        name: 'Tarun Sharma',
+        username: 'aryabhatta',
+        email: 'tarun.hive@hotmail.com'
+    },
+    
+    {
+        id: 2,
+        name: 'Abhay Rajput',
+        username: 'stupidsane',
+        email: 'devilmaker007@gmail.com'
+    },
+    
+    {
+        id: 3,
+        name: 'Saba Sarwar',
+        username: 'tss',
+        email: 'sabasarwar1997@gmail.com'
+    },
+    
+    {
+        id: 4,
+        name: 'Ashutosh Dwivedi',
+        username: 'ashu',
+        email: 'ashushivamdwivedi24197@gmail.com'
+    },
+    
+    {
+        id: 5,
+        name: 'Rishi Kumar',
+        username: 'mytechtalk',
+        email: 'mytechtalk99@gmail.com'
+    },
+    
+    {
+        id: 6,
+        name: 'Aastha Gupta',
+        username: 'panda',
+        email: 'aasthaaa1141@gmail.com'
+    },
+    
+    {
+        id: 7,
+        name: 'Pushpendra Gupta',
+        username: 'pushpi',
+        email: 'pushpendragupta032@gmail.com'
+    },
+   
+     {
+        id: 8,
+        name: 'Isha Khaleel',
+        username: 'isha',
+        email: 'ishakhaleel883@gmail.com'
+    },
+    
+    
+    {
+        id: 9,
+        name: 'Yogesh',
+        username: 'yogi',
+        email: 'yogeshwardalavi@gmail.com'
+    },
+    
+    {
+        id: 10,
+        name: 'Dipanshu Negi',
+        username: 'nikku',
+        email: 'nikksnegi@gmail.com'
+    }
+    
+    
+]
